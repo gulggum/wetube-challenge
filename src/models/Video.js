f8308af507ef69dda8f9a2d06d8dd7f242a9068e
gulggum <163222604+gulggum@users.noreply.github.com>
2024-07-16 17:58:55 +0900

    CRUD
    create,read,upload,delete

---
+++ src/models/Video.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+//해쉬테그를 미리 변환해주는 미들웨어(2)
+export const formatHashtags = (hashtags) =>
+  hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, required: true, default: 0 },
+    rating: { type: Number, required: true, default: 0 },
+  },
+});
+
+//해쉬테그를 미리 변환해주는 미들웨어(1)
+// videoSchema.pre("save", async function () {
+//  this.hashtags = this.hashtags[0]
+//    .split(",")
+//    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+//});
+
+const video = mongoose.model("Video", videoSchema);
+
+export default video;
